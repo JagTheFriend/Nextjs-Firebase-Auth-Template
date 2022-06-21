761d49cb6ec4731b4d0a6b875348c074b6d77f72
JagTheFriend <JagTheFriend12@gmail.com>
2022-06-21 22:10:11 +0200

    Implement context provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app';
+import { AuthContextProvider } from '../context/AuthContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // return <Component {...pageProps} />;
+  return (
+    <AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  );
 }
 
 export default MyApp;
