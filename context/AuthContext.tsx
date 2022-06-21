9fce3418e2c1dc85e95c8d7e768da35b72a2d3e8
JagTheFriend <JagTheFriend12@gmail.com>
2022-06-21 22:08:12 +0200

    Created an AuthContext

---
+++ context/AuthContext.tsx
@@ -0,0 +1,56 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import {
+  onAuthStateChanged,
+  signOut,
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import firebaseApp from '../firebase';
+
+const auth = getAuth(firebaseApp);
+export const AuthContext = createContext<any>({});
+
+export const useAuthContext = () => useContext(AuthContext);
+
+export const AuthContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+  console.log(user);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser({
+          uid: user.uid,
+          email: user.email,
+          displayName: user.displayName,
+        });
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const login = (email: string, password: string) => {
+    return signInWithPopup(auth, new GoogleAuthProvider());
+  };
+
+  const logout = async () => {
+    setUser(null);
+    await signOut(auth);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {loading ? null : children}
+    </AuthContext.Provider>
+  );
+};
