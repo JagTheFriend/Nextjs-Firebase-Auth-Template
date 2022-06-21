bf4b9064e789481ea004815b630e6ab90a4ea256
JagTheFriend <JagTheFriend12@gmail.com>
2022-06-21 22:01:33 +0200

    Updated file

--- firebase/index.ts
+++ firebase/index.ts
@@ -1,7 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getApps, initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,4 +11,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp =
+  // create a new app only if it doesn't already exists
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+export default firebaseApp;
