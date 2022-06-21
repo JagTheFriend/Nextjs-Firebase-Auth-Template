9821e90f729bdcafb9dda447364d72ee79ae8392
JagTheFriend <JagTheFriend12@gmail.com>
2022-06-21 20:47:01 +0200

    Fixed typo

--- environment.d.ts
+++ environment.d.ts
@@ -1,12 +1,12 @@
 declare global {
   export namespace NodeJS {
     export interface ProcessEnv {
-      PUBLIC_NEXT_FIREBASE_API_KEY: string;
-      PUBLIC_NEXT_FIREBASE_AUTH_DOMAIN: string;
-      PUBLIC_NEXT_FIREBASE_PROJECT_ID: string;
-      PUBLIC_NEXT_FIREBASE_STORAGE_BUCKET: string;
-      PUBLIC_NEXT_FIREBASE_MESSAGING_SENDER_ID: string;
-      PUBLIC_NEXT_FIREBASE_APP_ID: string;
+      NEXT_PUBLIC_FIREBASE_API_KEY: string;
+      NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+      NEXT_PUBLIC_FIREBASE_PROJECT_ID: string;
+      NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: string;
+      NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
+      NEXT_PUBLIC_FIREBASE_APP_ID: string;
     }
   }
 }
