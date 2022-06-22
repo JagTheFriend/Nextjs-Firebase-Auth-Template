eb637f76a2fb74916eb5c4a4f4ffbbe8bb651931
JagTheFriend <JagTheFriend12@gmail.com>
2022-06-22 21:24:55 +0200

    Redirect the user to login page if the user hasn't logged in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import { useAuthContext } from '../context/AuthContext';
 
 const Home: NextPage = () => {
   const { user } = useAuthContext();
+  const router = useRouter();
+  if (!user) {
+    router.push('/login');
+    return <>hello</>;
+  }
   return (
     <div>
       Uid: {user.uid} <br />
