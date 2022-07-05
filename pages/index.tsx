2035b0c4b2af5fdd189113e941417a784b810fa8
JagTheFriend <jagthefriend12@gmail.com>
2022-07-05 16:10:29 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,21 @@ import { useRouter } from 'next/router';
 import { useAuthContext } from '../context/AuthContext';
 
 const Home: NextPage = () => {
-  const { user } = useAuthContext();
+  const { user, logout } = useAuthContext();
   const router = useRouter();
   if (!user) {
     router.push('/login');
     return <>hello</>;
+  }
+  const logoutUser = async () => {
+    await logout()
   }
   return (
     <div>
       Uid: {user.uid} <br />
       Email: {user.email} <br />
       DisplayName: {user.displayName} <br />
+      <button onClick={logoutUser}>Logout</button>
     </div>
   );
 };
